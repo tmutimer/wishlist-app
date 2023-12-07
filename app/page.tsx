f0243e146edc49f9b396c4452d5c9ebafb684639
Tom Mutimer <tmutimer96@gmail.com>
2023-12-07 12:13:28 +0000

    Feat: add link to My List from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <div>Hello world!</div>
+      <Link href="/my-list">View My List</Link>
       
     </main>
   )
