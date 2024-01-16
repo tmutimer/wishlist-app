ac8b3d89bfbe1ed7fd7a99d3b7a9545f38136b0b
Tom Mutimer <tmutimer96@gmail.com>
2024-01-16 13:14:25 +0000

    Clean up addItemButton.tsx

--- components/addItemButton.tsx
+++ components/addItemButton.tsx
@@ -1,11 +1,6 @@
 'use client'
-import { useState } from "react";
-import ListItem from "./listItem";
-
 
 export default function AddItemButton({onPress}: {onPress: Function}) {
-
-    let [newItemName, setNewItemName] = useState(false)
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         onPress()
